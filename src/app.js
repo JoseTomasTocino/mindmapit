433aa65095ba6be18cc7e1d970c7f3343d33e765
José Tomás Tocino <theom3ga@gmail.com>
2016-01-02 03:28:39 +0100

    Cleaned up unused code (fixes #1)

--- src/app.js
+++ src/app.js
@@ -2,12 +2,10 @@ import TreeNode from './js/TreeNode.js';
 import * as utils from './js/Util.js';
 import parseList from './js/Parser.js';
 
-var YAML = require('yamljs');
-
 var vm = new Vue({
     el: '.content',
     data: {
-        sourceCode: 'Something something',
+        sourceCode: '',
         currentTree: undefined
     },
     methods: {
@@ -15,7 +13,6 @@ var vm = new Vue({
             console.log("Parsing...");
 
             try {
-                //var parsed = YAML.parse(this.sourceCode);
                 var parsed = parseList(this.sourceCode);
             } catch (err) {
                 console.log("Woops! Error parsing");
@@ -57,16 +54,14 @@ var vm = new Vue({
                 return;
             }
 
-            var shit = this.currentTree.draw();
-            canvas.width = shit.width + 25;
-            canvas.height = shit.height + 25;
+            var beautifulDrawing = this.currentTree.draw();
+            canvas.width = beautifulDrawing.width + 25;
+            canvas.height = beautifulDrawing.height + 25;
 
-            ctx.drawImage(shit, 25, 25);
+            ctx.drawImage(beautifulDrawing, 25, 25);
         }
     }
 });
-
-//window.addEventListener('resize', vm.regenerateDiagram);
 
 vm.sourceCode =
     `- Programming
@@ -103,52 +98,4 @@ vm.$watch('sourceCode', function (sourceCode) {
     vm.parseSource();
 });
 
-var maxDepth = 4;
-var maxChildren = 5;
-
-
-var generateRandomChildren = function (node, depth = 0) {
-    if (depth == maxDepth) return;
-
-    var numChildren = utils.getRandomInt(0, maxChildren);
-
-    for (var i = 0; i < numChildren; ++i) {
-        //var child = new TreeNode("Ch" + i + "d" + depth);
-        var child = new TreeNode(utils.getLoremIpsum(3));
-        generateRandomChildren(child, depth + 1);
-        node.addChild(child);
-    }
-};
-
-//generateRandomChildren(root);
-
-
-//var a = new TreeNode('Hijo 1');
-//var b = new TreeNode('Hijo 2');
-//var c = new TreeNode('Hijo 3');
-//
-//root.addChildren(a, b, c);
-//
-//a.addChildren(
-//    new TreeNode('Subh 1.1'),
-//    new TreeNode('Subh 1.2'),
-//    new TreeNode('Subh 1.3'),
-//    new TreeNode('Subh 1.4')
-//);
-//
-//b.addChildren(
-//    new TreeNode('Subh 2.1'),
-//    new TreeNode('Subh 2.2'),
-//    new TreeNode('Subh 2.2'),
-//    new TreeNode('Subh 2.2'),
-//    new TreeNode('Subh 2.2'),
-//    new TreeNode('Subh 2.2'),
-//    new TreeNode('Subh 2.2'),
-//    new TreeNode('Subh 2.2'),
-//    new TreeNode('Subh 2.2'),
-//    new TreeNode('Subh 2.3')
-//);
-
-//vm.currentTree = root;
 vm.parseSource();
-//vm.regenerateDiagram();
